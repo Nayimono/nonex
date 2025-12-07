c276be052456f8afea8c8804c7d03c6bcbd03f9e
weedswjnam <weeds.wj.nam@gmail.com>
2025-12-07 22:26:12 +0900

    [2025-12-07] [NAYIMO] : NEW

---
+++ src/test/java/com/nonex/NonexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
